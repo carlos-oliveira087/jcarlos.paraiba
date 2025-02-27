628a09bfca846b82e28e2f3d0b3b66a090cfa363
Carlos Oliveira <carlinhos.oneto0@gmail.com>
2025-02-27 15:56:39 -0300

    fix: resolvendo o problema de rotas 3

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/jcarlos_paraiba">
+    <BrowserRouter basename="/jcarlos.paraiba/">
       <App />
     </BrowserRouter>
   </StrictMode>
